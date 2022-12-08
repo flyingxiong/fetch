82b3733559d2e2d659e2a41292fa6f1e61191ca2
Shinyee Xiong <yingstudy@gmail.com>
2022-12-07 16:56:39 -0800

    create text and repo

---
+++ git.js
@@ -0,0 +1,15 @@
+// Git: version control system: to track versions/manage changes (branches) of your project
+// create by Linus Torvalds in 2005 (Linux)
+
+// repo (repository): project folder
+// local vs remote
+
+// remote: github/gitlab/bitbucket .... websites to host your repo online
+
+// check version: git --version
+
+// to create a new git repo:
+// git init
+// MAC: brew install git
+
+//git branch
